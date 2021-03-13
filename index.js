ceebda15990b959d9964527a1d6ae9339ec80ce3
Jeremy Wood <js.wood206@gmail.com>
2021-03-12 18:55:52 -0800

    commit tree d2224bb19eb8f626a74365c48c025dfaa8f05105

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => {
+        return driver.toLowerCase() === name.toLowerCase();
+    });
+};
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => {
+        return driver.substring(0, letters.length) === letters
+    });
+};
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name)
+}
